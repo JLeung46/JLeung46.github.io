5e5ec12bf47a28ba0bda6951fd1db289b90b9789
Jack Leung <jleungk46@gmail.com>
2017-01-31 15:06:21 -0500

    map colors

--- js/fertility_map.js
+++ js/fertility_map.js
@@ -56,26 +56,7 @@
 			    })
 			    .attr("text-anchor","middle")
 			    .attr('font-size','6pt');
-
-
-			   var legend = mapchart.selectAll("g.legend")
-				.append("g")
-			  	.attr("class", "legend");
-
-			  var ls_w = 20, ls_h = 20;
-
-			  legend.append("rect")
-			  .attr("x", 20)
-			  .attr("y", function(d, i){ return height - (i*ls_h) - 2*ls_h;})
-			  .attr("width", ls_w)
-			  .attr("height", ls_h)
-			  .style("fill", function(d, i) { return colorScale(d); })
-			  .style("opacity", 0.8);
-
-			  legend.append("text")
-			  .attr("x", 50)
-			  .attr("y", function(d, i){ return height - (i*ls_h) - ls_h - 4;})
-			  .text(function(d, i){ return legend_labels[i]; });			   
+			   
 		
 			});
 
